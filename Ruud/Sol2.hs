49fb342738669d4051fa9939cbb2b86b22a280c9
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2013-09-07 16:20:32 +0200

    added exercise 2.2, 2.4, 2.9

--- Ruud/Sol2.hs
+++ Ruud/Sol2.hs
@@ -5,3 +5,28 @@ where
 import GS
 import TAMO
 
+-- Exercise 2.2 (+2.4) (+2.9)
+--
+-- Question:	Make up the truth table for the exclusive version of or.
+--
+-- Answer:		+---+-------++-------+----------++----------------+
+--				|2.2|  2.2  ||  2.4  |   2.4    ||       2.9      |
+--				+---+-------++-------+----------++----------------+
+--				|P Q|P XOR Q||P <=> Q|!(P <=> Q)||(P XOR Q) XOR Q)|
+--				+---+-------++-------+----------++----------------+
+--				|t t|   f   ||   t   |    f     ||        t       |
+--				|t f|   t   ||   f   |    t     ||        t       |
+--				|f t|   t   ||   f   |    t     ||        f       |
+--				|f f|   f   ||   t   |    f     ||        f       |
+--				+---+-------++-------+----------++----------------+
+
+-- Exercise 2.4
+--
+-- Question:	Check that the truth table for exclusive or from Exercise 2.2 is equivalent
+--				to the table for !(P <=> Q). Conclude that the Haskell implementation of the
+--				function <+> for exclusive or in the frame below is correct.
+--
+-- Answer:		See exercise 2.2 for the truth table.
+--				The Haskell function is implemented to return true if x and y are not the same.
+--				This confirms the truth table.
+
--- Ruud/TAMO.hs
+++ Ruud/TAMO.hs
@@ -1,3 +1,44 @@
 module TAMO
 
 where
+
+(==>) :: Bool -> Bool -> Bool
+True ==> x = x
+False ==> x = True 
+
+(<=>) :: Bool -> Bool -> Bool
+x <=> y = x == y
+
+(<+>) :: Bool -> Bool -> Bool
+x <+> y = x /= y
+
+
+valid1 :: (Bool -> Bool) -> Bool
+valid1 bf = (bf True) && (bf False)
+
+valid2 :: (Bool -> Bool -> Bool) -> Bool
+valid2 bf = (bf True True) 
+		 && (bf True False)
+		 && (bf False True)
+		 && (bf False False)A
+
+valid3 :: (Bool -> Bool -> Bool -> Bool) -> Bool
+valid3 bf = and [ bf p q r | p <- [True,False],
+							 q <- [True,False],
+							 r <- [True,False]]
+valid4 :: (Bool -> Bool -> Bool -> Bool -> Bool) -> Bool
+valid4 bf = and [ bf p q r s | p <- [True,False],
+							   q <- [True,False],
+							   r <- [True,False],
+							   s <- [True,False]]
+excluded_middle :: Bool -> Bool
+excluded_middle p = p || not p
+
+p = True 
+q = False
+
+formula1 = (not p) && (p ==> q) <=> not (q && (not p))
+formula2 p q = ((not p) && (p ==> q) <=> not (q && (not p)))
+
+form1 p q = p ==> (q ==> p)
+form2 p q = (p ==> q) ==> p
