daf5c957cc0a4e550f996e74d246af32aa19e5f6
Ferry <Ferry>
2013-09-09 18:35:16 +0200

    Working on cnf having nightmares on dist

---
+++ Ferry/Haskell/assign2.hs
@@ -0,0 +1,29 @@
+
+import Week2
+
+
+contradiction :: Form -> Bool
+contradiction f = not (any (\ v -> eval v f) (allVals f))
+
+
+tautology :: Form -> Bool
+tautology f = all (\ v -> eval v f) (allVals f)
+
+-- logical entailment
+entails :: Form -> Form -> Bool
+entails f g = tautology (Impl f g)	
+
+-- logical equivalence
+
+equiv :: Form -> Form -> Bool
+equiv f g = tautology (Equiv f g)
+
+--cnf :: Form -> Form
+--cnf = nnf . arrowfree 
+
+cnf :: Form -> Form
+cnf (Prop x) = Prop x
+cnf (Cnj fs) = Cnj (map cnf fs)
+cnf (Dsj fs) = Dist (map cnf fs)
+
+
