ac14fd3e389d2ca9b453e45a75b902d2a3d50bb5
Ferry <Ferry>
2013-09-09 14:26:48 +0200

    First triangle version

--- Ferry/Haskell/triangle.hs
+++ Ferry/Haskell/triangle.hs
@@ -1,6 +1,17 @@
 -- isTriangle :: [int] -> bool
-isTriangle :: Int -> Int -> Int -> Bool
-isTriangle a b c  |  a + b > c = True
-                  |  b + c > a = True
-                  |  c + a > b = True
-                  |  otherwise = False
+
+data Shape = NoTriangle 
+		   | Equilateral
+           | Isosceles 
+           | Rectangular 
+           | Other 
+           deriving (Eq,Show)
+
+
+isTriangle :: Integer -> Integer -> Integer -> Shape
+
+isTriangle a b c  |  a == b && a == c  = Equilateral
+				  |  a == c || b == c  || a == b = Isosceles
+				  |  (a^2 + b^2 == c^2 || b^2 + c^2 == a^2 || a^2 + c^2 == b^2) = Rectangular 
+                  |  (a + b > c || b + c > a || c + a > b) =  Other
+                  |  otherwise = NoTriangle
