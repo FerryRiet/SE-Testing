cf30e8a4f1ea36a7a9e710149e85e3372e1a9b9f
Ferry <Ferry>
2013-09-12 18:25:39 +0200

    Creating tests

--- Ferry/Haskell/triangle.hs
+++ Ferry/Haskell/triangle.hs
@@ -1,5 +1,7 @@
 -- isTriangle :: [int] -> bool
 
+import Data.List
+
 data Shape = NoTriangle 
 		   | Equilateral
            | Isosceles 
@@ -10,8 +12,19 @@ data Shape = NoTriangle
 
 isTriangle :: Integer -> Integer -> Integer -> Shape
 
-isTriangle a b c  |  a == b && a == c  = Equilateral
+isTriangle a b c  |  (a + b < c || b + c < a || c + a < b) = NoTriangle
+				  |  a == b && a == c  = Equilateral
 				  |  a == c || b == c  || a == b = Isosceles
 				  |  (a^2 + b^2 == c^2 || b^2 + c^2 == a^2 || a^2 + c^2 == b^2) = Rectangular 
-                  |  (a + b > c || b + c > a || c + a > b) =  Other
-                  |  otherwise = NoTriangle
+                  |  otherwise = Other
+
+
+-- Testing approach
+-- Create input test sets  which will evaluate every booliean sub expression both True and
+-- False.  For the NoTriangle, Isosceles and Rectangular line need 8 input sets the Equilateral 
+-- line needs 4 different sets.   
+
+-- 
+t1 = [ isTriangle a b c | a <- [2..5] , b <- [2..5] , c <- [2..5]]
+t2 = [ isTriangle a b c | a <- [2..5] , b <- [2..5] , c <- [2..5], length (nub [a,b,c]) == 1 ]
+t3 = [ isTriangle a b c | a <- [2..5] , b <- [2..5] , c <- [2..5], length (nub [a,b,c]) == 2 ]
