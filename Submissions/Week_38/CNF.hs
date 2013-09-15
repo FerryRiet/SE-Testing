fb0511cac9b5d7e7976d3b9fe248209158aae396
Ferry <Ferry>
2013-09-15 22:12:41 +0200

    Removed Ruud CNF for Ferry's

--- Submissions/Week_38/CNF.hs
+++ Submissions/Week_38/CNF.hs
@@ -1,77 +1,70 @@
-module Week2.CNF 
+import Week2
+import Logic
 
-where
+--------------------------
 
-import Week2.Propositional
-
-
--- no precondition: should work for any formula. 
-arrowfree :: Form -> Form 
-arrowfree (Prop x) = Prop x 
-arrowfree (Neg f) = Neg (arrowfree f)
-arrowfree (Cnj fs) = Cnj (map arrowfree fs)
-arrowfree (Dsj fs) = Dsj (map arrowfree fs)
-arrowfree (Impl f1 f2) = 
-  Dsj [Neg (arrowfree f1), arrowfree f2]
-arrowfree (Equiv f1 f2) = 
-  Dsj [Cnj [f1', f2'], Cnj [Neg f1', Neg f2']]
-  where f1' = arrowfree f1
-        f2' = arrowfree f2
--- postcondition: output is arrow-free
-
--- precondition: input is arrow-free
-nnf :: Form -> Form 
-nnf (Prop x) = Prop x
-nnf (Neg (Prop x)) = Neg (Prop x)
-nnf (Neg (Neg f)) = nnf f
-nnf (Cnj fs) = Cnj (map nnf fs)
-nnf (Dsj fs) = Dsj (map nnf fs)
-nnf (Neg (Cnj fs)) = Dsj (map (nnf.Neg) fs)
-nnf (Neg (Dsj fs)) = Cnj (map (nnf.Neg) fs)
--- postcondition: NFF (f) returns NNF of f
-
--- precondition: input is arrow-free and in NNF
+-- precondition: input should be a valid form
+-- postcondition: CNF (a) returns CNF of a
 cnf :: Form -> Form
-cnf (Prop x) = Prop x
-cnf (Cnj fs) = Cnj (map cnf fs)
-cnf (Dsj (f1:f2:fs)) = dist f1 f2
-cnf f = f
--- postcondition: CNF (f) returns CNF of f
+cnf = cnf' . nnf . arrowfree -- Don't listen to Jimmy 
 
--- precondition: input are in CNF 
-dist :: Form -> Form -> Form
-dist (Cnj []) _ 		= Cnj []
-dist (Cnj [f1]) f2 		= dist f1 f2
-dist (Cnj (f1:fs)) f2	= Cnj [(dist f1 f2), (dist (Cnj fs) f2)]
-dist _ (Cnj []) 		= Cnj []
-dist f1 (Cnj [f2]) 		= dist f1 f2
-dist f1 (Cnj (f2:fs)) 	= Cnj [(dist f1 f2), dist f1 (Cnj fs)]
-dist f1 f2				= Dsj [f1,f2]
--- postcondition: DIST(f1,f2) returns CNF of Dsj [f1, f2]
+-- precondition: input forms is in nnf
+-- postcondition: CNF (NNF) returns CNF of NNF
+cnf' :: Form -> Form
+cnf' (Prop x) = Prop x
+cnf' (Neg  x) = Neg x        -- Do listen to Jimmy
+cnf' (Cnj fs) = Cnj (map cnf' fs)  
+cnf' (Dsj fs) = dist (map cnf' fs)
 
--- Tests
-fullCnf :: Form -> Form
-fullCnf = cnf . nnf . arrowfree
+-- precondition: input form is in cnf
+-- postcondition: returns form in cnf with the laws of distribution applied
+dist' :: Form -> Form ->Form
+dist' (Cnj f) p = Cnj (map (\x -> dist' x p) f)    
+dist' f (Cnj p) = Cnj (map (\x -> dist' f x) p)
+dist' f p = Dsj [f, p]  
 
-testCnf :: Form -> Bool
-testCnf f = equiv f (fullCnf f)
+-- precondition: all input forms are in cnf
+-- postcondition: returns form in cnf with the laws of distribution applied
+dist :: [Form] -> Form
+dist [f] = f
+dist (f:fs) = dist' f (dist fs)
 
--- Time spent, about 2 hours.
--- Tests results of CNF: 
---
--- *Week2> arrowfree form1
--- +(*(+(-1 2) +(--2 -1)) *(-+(-1 2) -+(--2 -1)))
--- *Week2> equiv (form1) (arrowfree form1)
--- True
--- *Week2> nnf . arrowfree $ form1
--- +(*(+(-1 2) +(2 -1)) *(*(1 -2) *(-2 1)))
--- *Week2> equiv form1 (nnf . arrowfree $ form1)
--- True
--- *Week2> (cnf . nnf . arrowfree $ form1)
--- *(*(*(+(+(-1 2) 1) +(+(-1 2) -2)) *(+(+(-1 2) -2) +(+(-1 2) 1))) *(*(+(+(2 -1) 1) +(+(2 -1) -2)) *(+(+(2 -1) -2) +(+(2 -1) 1))))
--- *Week2> equiv form1 (cnf . nnf . arrowfree $ form1)
--- True
+-- precondition: a valid form
+-- postcondition: returns true if "valid form" is equivalent with CNF("valid form")
+testCNF :: Form -> Bool
+testCNF f = equiv f g
+		    where g = cnf f
 
--- Run tests on predefined (and used) forms
-testCNF1 = all testCnf [form1, form2, form3]
-testCNF2 = all testCnf (allTaut ++ allCont ++ allSatis)
+
+t1 = testCNF (Dsj [Dsj [p,q],q])
+
+{-
+Coding the first version took +- 3 hours, id did not work at all 
+missing was the Neg operator from the CNF algorithm. Took me 1 hour to
+understand the Haskell not matching error, and the missing case.
+-}
+
+{- Dist check from lecture slides
+First case uses equivalence of (p ^ q) _ r and (p _ r) ^ (q _ r).
+Second case uses equivalence of p _ (q ^ r) and (p _ q) ^ (p _ r).
+-}
+
+dc1 = dist' p (Cnj [q, r]) == Cnj [Dsj [p, q], Dsj [p, r]]
+dc2 = dist' (Cnj [q, r]) p == Cnj [Dsj [q, p], Dsj [r, p]]
+
+{-
+Testing
+
+run test set composed of satisfiables, contradictions, and tautologies and test if there CNF is equivalent
+with the original function.
+
+-}
+
+distResults = all (\x -> x) [dc1,dc2]
+
+cnfResults = all testCNF [s1,s2,s3,s4,s5,s6,c1,c2,c3,c4,Logic.t1,t2,t3,t4,t5,t6]
+
+--nodist :: (Form,Form) -> Form
+--nodist Cnj f j   = j
+--nodisk f Cnj j  = f
+--nodisk f j       = Dsj f j
