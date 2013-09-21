a9ce271673bcfa637ca50ae442619c843d9f6ed1
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2013-09-22 00:37:01 +0200

    finished #6 and #7 of week 3. (test CNF & FirstOrderLogic formula generator) -- Added some old files (from week2) because they are needed in some functions or tests. -- Not done (yet?!): Bonus Exercise.

--- Ruud/Week3/Assignments.hs
+++ Ruud/Week3/Assignments.hs
@@ -3,34 +3,16 @@ module Assigments
 where 
 
 import Week2
+import Propositional
+import PropositionalTests
+
 import Data.List
 import Data.Char
 import System.Random
+
 import Week3
 import Techniques
+import Permutation
+import TestCNF
+import FOL
 
--- genIntList creates a list of 'n' Ints from zero to 'm'
-genIntList :: IO [Int]
-genIntList = do 
-        n <- getRandomInt 5
-        m <- getRandomInt 5
-        getRandomInts n m 
-                   
--- getRandomInts creates a list of 'n' Ints from zero to 'm'
-getRandomInts :: Int -> Int -> IO [Int]
-getRandomInts 0 m = return []
-getRandomInts n m = do
-        f <- getRandomInt m
-        fs <- getRandomInts (n-1) m
-        return (f:fs)
-
--- a permutation is of the same length
--- a permutation contains the same numbers but may appear in different order, so the ordered lists of the items are the same
--- the sum of a permutation is the same
-isPermutation :: Eq a => [a] -> [a] -> Bool
-isPermutation [] []             = True
-isPermutation [] _              = False
-isPermutation _ []              = False
-isPermutation (x:xs) (y:ys)  
-        | x == y = isPermutation xs ys
-        | otherwise = isPermutation xs (delete x (y:ys))
