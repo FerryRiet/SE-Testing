083246ba0498c665a5182db9a8a61063bc616b92
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2013-09-21 08:44:40 +0200

    simplified isPermutation (now only one recursive function) -- use default function to remove first occurange: delete

--- Ruud/Week3/Assignments.hs
+++ Ruud/Week3/Assignments.hs
@@ -28,17 +28,9 @@ getRandomInts n m = do
 -- a permutation contains the same numbers but may appear in different order, so the ordered lists of the items are the same
 -- the sum of a permutation is the same
 isPermutation :: Eq a => [a] -> [a] -> Bool
+isPermutation [] []             = True
 isPermutation [] _              = False
 isPermutation _ []              = False
-isPermutation (xs) (ys)         = compareLists xs ys
-
--- keep removing items till there are none left
-compareLists :: Eq a => [a] -> [a] -> Bool
-compareLists [] []              = True
-compareLists [] _               = False
-compareLists _ []               = False
-compareLists (x:xs) (ys)        = compareLists xs (removeItem x ys) 
-
-removeItem :: Eq a => a -> [a] -> [a]
-removeItem x (y:ys) | x == y    = ys
-                    | otherwise = y : removeItem x ys
+isPermutation (x:xs) (y:ys)  
+        | x == y = isPermutation xs ys
+        | otherwise = isPermutation xs (delete x (y:ys))
