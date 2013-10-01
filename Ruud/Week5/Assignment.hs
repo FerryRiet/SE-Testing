23c54cd488e1f006cba971d684d596b8d821318f
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2013-10-01 09:59:49 +0200

    mergeSrt assertion

--- Ruud/Week5/Assignment.hs
+++ Ruud/Week5/Assignment.hs
@@ -7,8 +7,20 @@ import Week4
 import Week5
 import RandomSudoku
 
-{- Precondition -}
 mergeSrt :: Ord a => [a] -> [a]
 mergeSrt [] = []
 mergeSrt (x:xs) = merge [x] (mergeSrt xs)
-{- Postcondition: list is empty or sorted -}
+
+{- Length of input and output of the list is the same -}
+lengthProp :: Ord a => [a] -> [a] -> Bool
+lengthProp xs ys = length xs == length ys
+
+{- The output list is the sorted version of the input -}
+sortedOutputProp :: Ord a => [a] -> [a] -> Bool
+sortedOutputProp xs ys = sort xs == ys
+
+mergeSrtA :: Ord a => [a] -> [a]
+mergeSrtA = assert1 lengthProp $ assert1 sortedOutputProp mergeSrt
+
+-- indication of time spent: about 30 minutes.
+
