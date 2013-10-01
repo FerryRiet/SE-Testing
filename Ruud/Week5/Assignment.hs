c5dab610be84c3449c86da10558999a7405249dc
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2013-10-01 10:18:23 +0200

    implemented mergeSrt using split

--- Ruud/Week5/Assignment.hs
+++ Ruud/Week5/Assignment.hs
@@ -24,3 +24,26 @@ mergeSrtA = assert1 lengthProp $ assert1 sortedOutputProp mergeSrt
 
 -- indication of time spent: about 30 minutes.
 
+mergeSrtSplit :: Ord a => [a] -> [a]
+mergeSrtSplit []  = []
+mergeSrtSplit [x] = [x]
+mergeSrtSplit xs  = merge (mergeSrtSplit (fst ys)) (mergeSrtSplit (snd ys))
+        where ys = split xs
+
+split :: [a] -> ([a],[a])
+split xs = let 
+             n = (length xs) `div` 2 
+           in 
+             (take n xs, drop n xs)
+
+sublistProp1 :: Eq a => [a] -> [a] -> Bool
+sublistProp1 xs ys = 
+  sublist xs ys
+
+mergeSrtSplitA :: Ord a => [a] -> [a]
+mergeSrtSplitA = assert1 lengthProp 
+        $ assert1 sortedOutputProp 
+        $ assert1 sublistProp1
+        mergeSrtSplit
+
+-- indication of time spent: about 30 minutes.
