6b414780cc7a31eaa9257a91141f73fcdfb75f07
Ruud van der Weijde <ruudvanderweijde@gmail.com>
2013-10-08 14:25:29 +0200

    first 2 assignments of week6

---
+++ Ruud/Week6/Assignment.hs
@@ -0,0 +1,98 @@
+module Assignment
+
+where 
+
+import Week6
+import Lab6
+import Text.Printf
+import Control.Exception
+import System.CPUTime
+
+{- Exercise 1:
+    Created exM 
+-}
+exM' :: Integer -> Integer -> Integer -> Integer
+exM' _ 0 _ = 1
+exM' b e m
+    | even e    = multM z z m
+    | otherwise = multM b (multM z z m) m
+    where z = exM' b (div e 2) m
+
+-- simple test to check the logic
+test1 = all test1' [0..10000]
+test1' x = (x^33) `mod` 5 == (x^32 `mod` 5) * (x `mod` 5)
+test2 = all test2' [0..10000]
+test2' x = (x^33) `mod` 5 == (rem ((x^16 `mod` 5) * (x^16 `mod` 5)) 5) * (x `mod` 5)
+
+{-- Measure functions: Results below -}
+testNumbersStr = ["m1,m2,m3","m2,m3,m4","m3,m4,m5","m4,m5,m6","m5,m6,m7","m6,m7,m8","m7,m8,m9","m8,m9,m10","m9,m10,m11","m10,m11,m12","m11,m12,m13","m12,m13,m14","m13,m14,m15","m14,m15,m16","m15,m16,m17","m16,m17,m18","m17,m18,m19","m18,m19,m20","m19,m20,m21","m20,m21,m22","m21,m22,m23","m22,m23,m24","m23,m24,m25"]
+testNumbers    = [[m1,m2,m3],[m2,m3,m4],[m3,m4,m5],[m4,m5,m6],[m5,m6,m7],[m6,m7,m8],[m7,m8,m9],[m8,m9,m10],[m9,m10,m11],[m10,m11,m12],[m11,m12,m13],[m12,m13,m14],[m13,m14,m15],[m14,m15,m16],[m15,m16,m17],[m16,m17,m18],[m17,m18,m19],[m18,m19,m20],[m19,m20,m21],[m20,m21,m22],[m21,m22,m23],[m22,m23,m24],[m23,m24,m25]]
+
+measureExMs :: IO ()
+measureExMs = measureExMs' $ zip testNumbersStr testNumbers
+
+measureExMs' :: [(String,[Integer])] -> IO ()
+measureExMs' []     = putStrLn "All done."
+measureExMs' (x:xs) = do 
+        measureExM (fst x) (snd x)
+        measureExMs' xs
+
+-- returns the execution time of the functions
+measureExM :: String -> [Integer] -> IO ()
+measureExM s (b:e:m:xs) = do
+    printf "Test '%s'" s
+    test1 <- timeToString  ((exM b e m) `seq` return ())
+    putStr (" exM:" ++ test1)
+    test2 <- timeToString  ((exM' b e m) `seq` return ())
+    putStr (" |exM':" ++ test2)
+    if b <= m6 then do
+        test3 <- timeToString ((expM b e m) `seq` return ())
+        putStrLn (" | expM:" ++ test3)
+    else 
+        putStrLn " | expM': Skipped."
+
+{- borrowed this function from haskell.org and modified it.
+   Link: http://www.haskell.org/haskellwiki/Timing_computations -}
+timeToString :: PrintfType b => IO t -> IO b
+timeToString a = do
+    start <- getCPUTime
+    v <- a
+    end   <- getCPUTime
+    let diff = (fromIntegral (end - start)) / (10^12)
+    return $ printf "%0.3f sec" (diff :: Double)
+
+{- Exercise 2
+    Result of measureExMs:
+
+        Test 'm1,m2,m3' exM: 0.000 sec |exM': 0.000 sec |expM: 0.000 sec
+        Test 'm2,m3,m4' exM: 0.000 sec |exM': 0.000 sec |expM: 0.000 sec
+        Test 'm3,m4,m5' exM: 0.000 sec |exM': 0.000 sec |expM: 0.000 sec
+        Test 'm4,m5,m6' exM: 0.000 sec |exM': 0.000 sec |expM: 0.000 sec
+        Test 'm5,m6,m7' exM: 0.000 sec |exM': 0.000 sec |expM: 0.031 sec
+        Test 'm6,m7,m8' exM: 0.000 sec |exM': 0.000 sec |expM: 0.281 sec
+        Test 'm7,m8,m9' exM: 0.000 sec |exM': 0.000 sec | expM': Skipped.
+        Test 'm8,m9,m10' exM: 0.000 sec |exM': 0.000 sec | expM': Skipped.
+        Test 'm9,m10,m11' exM: 0.000 sec |exM': 0.000 sec | expM': Skipped.
+        Test 'm10,m11,m12' exM: 0.016 sec |exM': 0.000 sec | expM': Skipped.
+        Test 'm11,m12,m13' exM: 0.000 sec |exM': 0.000 sec | expM': Skipped.
+        Test 'm12,m13,m14' exM: 0.000 sec |exM': 0.000 sec | expM': Skipped.
+        Test 'm13,m14,m15' exM: 0.016 sec |exM': 0.000 sec | expM': Skipped.
+        Test 'm14,m15,m16' exM: 0.031 sec |exM': 0.062 sec | expM': Skipped.
+        Test 'm15,m16,m17' exM: 0.062 sec |exM': 0.062 sec | expM': Skipped.
+        Test 'm16,m17,m18' exM: 0.109 sec |exM': 0.125 sec | expM': Skipped.
+        Test 'm17,m18,m19' exM: 0.234 sec |exM': 0.218 sec | expM': Skipped.
+        Test 'm18,m19,m20' exM: 0.359 sec |exM': 0.359 sec | expM': Skipped.
+        Test 'm19,m20,m21' exM: 1.014 sec |exM': 1.030 sec | expM': Skipped.
+        Test 'm20,m21,m22' exM: 2.324 sec |exM': 2.356 sec | expM': Skipped.
+        Test 'm21,m22,m23' exM: 3.791 sec |exM': 3.822 sec | expM': Skipped.
+        Test 'm22,m23,m24' exM: 8.096 sec |exM': 8.112 sec | expM': Skipped.
+        Test 'm23,m24,m25' exM: 16.723 sec |exM': 16.770 sec | expM': Skipped.
+        All done.
+        (66.16 secs, 1541471992 bytes)
+-}
+
+{- Exercise 3
+
+-}
+composites :: [Integer]
+composites = []
